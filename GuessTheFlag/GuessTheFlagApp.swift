50ecb0c8142fe309fce694bafe0dead12cf5ba18
Pedro Henriques <pedromnh@gmail.com>
2021-12-04 01:23:16 +0000

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Pedro Henriques on 04/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
